25fd2e834064ee2bf6b39b46c11d1fba720b700d
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-10-19 17:21:13 -0300

    feat: first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(createVuetify({
+  components,
+  directives,
+}))
+app.use(router)
+
+app.mount('#app')
