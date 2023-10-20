098a48bccf4b0f60fa10843f9c4b33e0517da3fb
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-10-19 23:01:38 -0300

    feat: adding toggle theme component

---
+++ src/components/Buttons/ToggleTheme/ToggleTheme.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-btn variant="text" @click="toggleTheme">
+      <div v-if="theme.global.name.value === 'light'">
+        <v-icon>mdi-weather-night</v-icon>
+      </div>
+      <div v-else>
+        <v-icon>mdi-weather-sunny</v-icon>
+      </div>
+    </v-btn>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme();
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
